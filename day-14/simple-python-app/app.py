e343d7c521fce57e691d49dee557839bb70b7cdb
LakshyaGulenia <lakshyagulenia451@gmail.com>
2025-05-05 19:26:15 +0530

    change in app.py file

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, aws codebuild!'
+    return 'Hello, aws codebuild from vscode!'
 
 if __name__ == '__main__':
     app.run()
